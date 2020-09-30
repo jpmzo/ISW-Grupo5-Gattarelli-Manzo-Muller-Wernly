82c970b94723420657e34e2b6f9345df0b4f383d
jpmzo <71462520+jpmzo@users.noreply.github.com>
2020-09-30 19:04:08 -0300

    Add files via upload

---
+++ ISW-Grupo5-Gattarelli-Manzo-Muller-Wernly/README.md
@@ -0,0 +1,9 @@
+# ISW-TP-Gattarelli-Manzo-Muller-Wernly
+
+Trabajo práctico nº1 de ISW 2020
+
+Probando
+
+Probando 2
+
+Probando 3
---
+++ ISW-Grupo5-Gattarelli-Manzo-Muller-Wernly/src/pages/home/home.ts
@@ -0,0 +1,73 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { ANIMALES } from "../../data/data.animales";
+import { Animal } from "../../interfaces/animal.interface";
+import { Refresher } from "ionic-angular";
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  animales:Animal[] = [];
+  audio = new Audio();
+  audioTiempo: any;
+
+  constructor() {
+
+    this.animales = ANIMALES.slice(0);
+
+  }
+
+  reproducir(animal:Animal){
+
+    this.pausar_audio(animal);
+    if(animal.reproduciendo){
+      animal.reproduciendo = false;
+      return;
+    }
+
+    console.log(animal);
+
+    this.audio.src = animal.audio;
+
+    this.audio.load();
+    this.audio.play();
+
+    animal.reproduciendo = true;
+
+    this.audioTiempo = setTimeout( ()=> animal.reproduciendo = false, animal.duracion * 1000);
+
+  }
+
+  private pausar_audio( animalSel:Animal ){
+
+    clearTimeout(this.audioTiempo);
+    this.audio.pause();
+    this.audio.currentTime = 0;
+
+    for(let animal of this.animales){
+      if(animal.nombre != animalSel.nombre){
+        animal.reproduciendo = false;
+      }
+    }
+
+  }
+
+  borrar_animal(idx:number){
+
+    this.animales.splice(idx,1);
+
+  }
+
+  recargar_animales(refresher:Refresher ){
+    console.log("Inicio del refresh");
+    setTimeout( ()=>{
+        console.log("Terminó el refresh");
+        this.animales = ANIMALES.slice(0);
+        refresher.complete();
+    },1500)
+  }
+
+}
